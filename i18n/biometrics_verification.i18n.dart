83303d6370dab5dcf678fbec0373c3978aaf0a35
Julio L R Monteiro <juliolrmonteiro@gmail.com>
2021-02-07 20:57:22 -0300

    Adding pt_br translation

--- i18n/biometrics_verification.i18n.dart
+++ i18n/biometrics_verification.i18n.dart
@@ -5,12 +5,12 @@ extension Localization on String {
 static var _t = Translations.byLocale("en_us") +
     {
       "es_es": {
-        'Biometrics Disabled': 'Spanish Translation goes here!',
-        'Loading your SEEDS Wallet...': '',
-        'Initializing Biometrics': '',
-        'Enable Settings': '',
-        'Try Again': '',
-        'Use Passcode': ''
+        'Biometrics Disabled': 'Biometria Desativada',
+        'Loading your SEEDS Wallet...': 'Carregando sua Carteira SEEDS',
+        'Initializing Biometrics': 'Inicializando Biometria',
+        'Enable Settings': 'Habilitar Configurações',
+        'Try Again': 'Tente Novamente',
+        'Use Passcode': 'Usar Senha'
       }
     };
   String get i18n => localize(this, _t);
